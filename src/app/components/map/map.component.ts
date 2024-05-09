0026ea6a05934f35082138b8706ab1195de45e5f
JoffreC <joffre.condor@epn.edu.ec>
2024-05-09 15:55:02 -0500

    first commit

--- src/app/components/map/map.component.ts
+++ src/app/components/map/map.component.ts
@@ -12,33 +12,42 @@ export class MapComponent implements OnInit {
 
   private svg: any;
   private margin = 50;
-  private width = 1000 - (this.margin * 2);
-  private height = 600 - (this.margin * 2);
+  private width = 2000 - (this.margin * 2);
+  private height = 800 - (this.margin * 2);
   private scale = Math.min(this.width, this.height);
   private geoJson: any;
+
 
   constructor() { }
 
   private createSvg(): void {
+
+// Definir una escala de color ord
     var projection = d3.geoMercator()
-      .center([-80,-2])
+      .center([-87,0])
       .scale(4000)
       // .rotate([0, 0]);
 
     let geoGenerator: any = d3.geoPath()
       .projection(projection);
-    json('assets/custom.geo.json')
+    json('assets/ecuador.geojson')
       .then(data => {
         this.geoJson = data
+        var provinces = this.geoJson.features.map((d: { properties: { dpa_despro: string; }; }) => d.properties.dpa_despro);
+        var colorScale = d3.scaleOrdinal()
+          .domain(provinces)
+          .range(["#c7d3ec", "#a5b8db", "#879cc4", "#677795", "#5a6782"])
         this.svg = d3.select('figure#map')
           .append("svg")
           .attr("width", this.width + (this.margin * 2))
           .attr("height", this.height + (this.margin * 2))
           .append("g")
+          .style("background-color", "lightblue")
           .selectAll('path')
           .data(this.geoJson.features)
           .join('path')
-          .attr('d', geoGenerator);
+          .attr('d', geoGenerator)
+          // .attr("fill", (d: any) => (colorScale(d)))
       })
 
   }
